d8daf89f7a6a8fabefead7bbb084d6a7d7ed5c4b
petrleba <131344580+petrleba@users.noreply.github.com>
2025-12-09 21:46:24 +0100

    switch

---
+++ kapitola07/main02.py
@@ -0,0 +1,23 @@
+# IMPORT MODULŮ, KNIHOVEN A METOD
+from machine import Pin
+from utime import sleep
+
+# NASTAVENÍ MIKROKONTROLERU
+led = Pin(0, Pin.OUT)    # nastavíme pin číslo 0 jako výstupní
+
+# nastavíme pin číslo 28 jako vstupní 
+# a jeho vstup ošetřený pull up rezistorem
+tlacitko = Pin(28, Pin.IN, Pin.PULL_DOWN)  
+
+led.value(0)
+
+while True:
+    if (tlacitko.value() == 1):  # zjistíme hodnotu na pinu 28. Je tlačítko sepnuto?
+        led.value(1)
+        sleep(0.3)
+        led.value(0)
+        sleep(0.3)
+    else:
+        led.value(0)
+
+    sleep(0.1)      # uspí mikrokontroler na daný počet sekund
